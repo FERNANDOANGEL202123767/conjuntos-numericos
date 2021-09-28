0fe6b7ad10dbdf2fe32997898a64687361cbd447
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2021-09-28 16:08:16 -0500

    demostracion carga de archivo
    primera demostracion

---
+++ clasificacion de numeros.m
@@ -0,0 +1,66 @@
+% octave srcipt
+% title             :clasificacion de los numeros
+% description       :script para recordar conceptos de numeros
+% author            :fernando angel garcia hernandez
+% date              :20210928
+% version           :1
+% usage             :octave C:\Users\Lenovo L560\Desktop
+% notes             :octave>clasificacionNumeros             
+%                   :requiere aplicacion octave, usar linea de comandos
+%                   :http://octaveintro.readthedocs.io/en/latest/index.html
+
+clear
+c_numeros_Naturales = `?= {1, 2, 3, .... n} si n > 0`;
+c_numeros_Enteros = `?= { -n..., -2, -1, 0, 1, 2,..n }`;
+c_numeros_Racionales = `?= { m/n donde m,n ?? n ? 0}`;
+c_numeros_Irracionales = `I = {?n que no puede ser expresada como ? todas las raices que no son exactas`; 
+c_numeros_Reales = `?= { I, ? ?, ?}`;
+
+% propiedades de los numeros,sean a,b,c,d,e ?? 
+
+% propiedades de ?(cerradura)
+p_cerradura = `a + b ??;
+p_cerradura2 = `ab ??;
+p_cerradura3 = `7 + 9 ??;
+p_cerradura4 = `?= pertenencia`;
+a=
+b=
+a+b
+a*b
+
+% propiedad asociativa
+p_asociativa = `a + (b + c )`;
+p_asociativa2 = `a (b c ) = ( a b ) c`;
+p_asociativa3 = `3 + ( 8 - 10 ) = ( 8 + 3 ) - 10`;
+
+% propiedad conmutitativa
+p_conmutativa = `a + b = b + a`;
+p_conmutativa2 = ``
+
+% propiedad distributiva
+p_distributiva = ``
+
+%neutro aditivo
+p_neutroA = ``
+p_neutroA2 = ``
+
+% neutro multiplicativo
+p_neutroM = ``
+
+% inverso aditivo
+p_inversoA = ``
+
+% inverso multiplicativo o reciproco
+p_inversoM = ``
+
+% PROPIEDAD TRANSITIVA (| enconces)
+p_transitiva = ``
+p_transitiva2 = ``
+
+% tricotomia (raiz del algebra) siempre se pueden comprar
+p_tricotomia = ``
+p_tricotomia2 = ``
+p_tricotomia3 = ``
+
+% signos de agrupacion
+s_agrupacion = ``
